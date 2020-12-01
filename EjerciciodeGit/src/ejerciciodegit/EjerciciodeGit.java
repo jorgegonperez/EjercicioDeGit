9e8f33f9c2a47b73eaa37c0d8c560ed7e8edefa2
dam <dam@172.26.47.13>
2020-12-01 12:09:19 +0100

    CLONADO DE PROYECTO

--- EjerciciodeGit/src/ejerciciodegit/EjerciciodeGit.java
+++ EjerciciodeGit/src/ejerciciodegit/EjerciciodeGit.java
@@ -17,7 +17,7 @@ public class EjerciciodeGit {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Primer cambio");
-   
+        
     }
     
 }
